733a05e2c2155318241d2cf2be8b765b63584cce
Sarthak Agrawal <sarthak.agrawal76@gmail.com>
2016-03-20 04:38:46 +0530

    Update auth.js

--- config/auth.js
+++ config/auth.js
@@ -1,7 +1,7 @@
 module.exports = {
   'facebookAuth' : {
-    'clientID' : '1750551035178275',
-    'clientSecret' : '059d5389f14ff42260e1b801f46384d9',
+    'clientID' : '',
+    'clientSecret' : '',
     'callbackURL' : 'http://localhost:8080/auth/facebook/callback',
     'profileFields' : ['emails','first_name', 'last_name']
   }
